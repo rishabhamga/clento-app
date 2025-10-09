482c7e8353c2987f7fc0a0f040c6d7f4f9da1f4b
Yash verma <yashverma2367@gmail.com>
2025-10-09 16:12:23 +0530

    Bug fix

---
+++ src/helpers/helpers.ts
@@ -0,0 +1,26 @@
+
+export const orgToSmartLeadCampaignMap = {
+    '6f462221-2f14-48a3-91d1-b60ba824caf9': [//Revenue sage
+        '2510990',
+        '2510844'
+    ],
+    'b9580756-519d-44d4-a3a9-9622c62816a3': [//Alloroots
+        '2444463',
+        '2444437',
+        '2444372'
+    ],
+    '784b9227-61c5-47c9-97ac-d46233cd3a69': [ // Altech
+        '2440388',
+        '2440266',
+        '2440099'
+    ],
+    '5b733d24-8be8-4c2b-87c9-fcf50eadb796': [ // Supreme
+        '2444335',
+        '2444280'
+    ],
+    '8be53b58-d55b-4b58-b90f-a13e9815bbab': [//Upriver
+        '2440779',
+        '2440699'
+    ],
+    // @todo graphketing
+}
